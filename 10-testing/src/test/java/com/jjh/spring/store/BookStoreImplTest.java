b91944fb69eb2c0434a6334bd94f07ae6ea816c2
John Hunt <john.hunt10@gmail.com>
2020-05-19 14:59:25 +0100

    Changed config specification

---
+++ 10-testing/src/test/java/com/jjh/spring/store/BookStoreImplTest.java
@@ -0,0 +1,44 @@
+package com.jjh.spring.store;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { TestConfig.class} )
+class BookStoreImplTest {
+
+    @Autowired
+    private BookStore bookstore;
+
+    @Test
+    void checkNameOfBookstore() {
+        String name = bookstore.getName();
+        Assertions.assertEquals("My Company", name);
+    }
+
+    @Test
+    void getBooks() {
+        int number = bookstore.getBooks().size();
+        Assertions.assertEquals(2, number, "There should be 2 books in the bookstore");
+    }
+
+    @Test
+    void getMessage() {
+        String message = bookstore.getMessage();
+        Assertions.assertEquals("Welcome", message);
+    }
+
+    @Test
+    void getBookByTitle() {
+        Book book = bookstore.getBookByTitle("Java and Spring");
+        Assertions.assertNotNull(book);
+        Assertions.assertEquals("123", book.getIsbn());
+    }
+}
