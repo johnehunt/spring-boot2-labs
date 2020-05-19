e067626ff0f6fd964d3f1db9c1a1a6cf5bc89450
John Hunt <john.hunt10@gmail.com>
2020-05-19 14:56:10 +0100

    Sample of using Spring and JUnit testing

---
+++ 10-testing/src/test/java/com/jjh/spring/store/TestConfig.java
@@ -0,0 +1,9 @@
+package com.jjh.spring.store;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan(basePackages = {"com.jjh.spring.beans","com.jjh.spring.store"})
+public class TestConfig {
+}
