96aabde86a9ec165522f70057830e2be4b689aeb
John Hunt <john.hunt10@gmail.com>
2020-05-19 12:21:47 +0100

    Added sample beans lab solution

---
+++ 02-beans/src/main/java/com/jjh/beans/DateServiceImpl.java
@@ -0,0 +1,24 @@
+package com.jjh.beans;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class DateServiceImpl implements DateService {
+	
+	private Date date;
+	
+	public DateServiceImpl() {
+		date = new Date();
+	}
+
+	@Override
+	public String getDate() {
+		return date.toString();
+	}
+
+}
---
+++ 02-beans/src/main/java/com/jjh/helloworld/HelloworldApplication.java
@@ -0,0 +1,39 @@
+package com.jjh.helloworld;
+
+import com.jjh.beans.DateService;
+import com.jjh.beans.MessageService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.jjh.beans")
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		System.out.print(("Main - Starting the application"));
+		ApplicationContext ctx = SpringApplication.run(HelloworldApplication.class, args);
+
+		System.out.println("Main - requesting MessageService");
+		MessageService service = ctx.getBean(MessageService.class);
+		System.out.println(service.getMessage());
+
+		System.out.println("Main - requesting DateService");
+		DateService ds1 = ctx.getBean(DateService.class);
+		System.out.println(ds1.getDate());
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Main - requesting DateService (again)");
+		DateService ds2 = ctx.getBean(DateService.class);
+		System.out.println(ds2.getDate());
+
+		System.out.print(("Main - Application startup completed"));
+	}
+
+}
