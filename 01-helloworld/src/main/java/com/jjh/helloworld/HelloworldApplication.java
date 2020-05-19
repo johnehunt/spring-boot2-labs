255ab85337dfff0d18176dfd6f18d083beda1954
John Hunt <john.hunt10@gmail.com>
2020-05-19 12:07:17 +0100

    Simple lab solution

---
+++ 01-helloworld/src/main/java/com/jjh/helloworld/HelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.jjh.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		System.out.print(("Starting the application"));
+		SpringApplication.run(HelloworldApplication.class, args);
+		System.out.print(("Application startup completed"));
+	}
+
+}
