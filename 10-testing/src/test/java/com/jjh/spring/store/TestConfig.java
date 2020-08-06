5fb034be4d8a0868cca41327efd9a06526248aae
John Hunt <john.hunt10@gmail.com>
2020-08-06 17:27:26 +0100

    Removed blank lines

--- 10-testing/src/test/java/com/jjh/spring/store/TestConfig.java
+++ 10-testing/src/test/java/com/jjh/spring/store/TestConfig.java
@@ -5,5 +5,4 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootConfiguration
 @ComponentScan(basePackages = {"com.jjh.spring.beans","com.jjh.spring.store"})
-public class TestConfig {
-}
+public class TestConfig { }
