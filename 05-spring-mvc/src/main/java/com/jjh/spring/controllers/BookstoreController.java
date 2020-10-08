0fd98d9841545f9841bb4bcbb28d0fbff842d3bd
John Hunt <john.hunt10@gmail.com>
2020-10-08 15:39:39 +0100

    CHanged to use model passed into controller method

--- 05-spring-mvc/src/main/java/com/jjh/spring/controllers/BookstoreController.java
+++ 05-spring-mvc/src/main/java/com/jjh/spring/controllers/BookstoreController.java
@@ -3,17 +3,21 @@ package com.jjh.spring.controllers;
 import com.jjh.spring.store.BookStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Controller
 @RequestMapping("books")
 public class BookstoreController {
 
-	@Autowired
 	private BookStore bookstore;
+
+	@Autowired
+	public BookstoreController(BookStore bookstore) {
+		this.bookstore = bookstore;
+	}
 
 	public BookstoreController() {
 		System.out.println("BookstoreController.<init>()");
@@ -27,19 +31,18 @@ public class BookstoreController {
 	// http://localhost:8080/books/list
 	
 	@RequestMapping("/list")
-	public ModelAndView getAllBooks() {
+	public String getAllBooks(Model model) {
 		System.out.println("BookstoreController.getAllBooks()");
-		ModelAndView mav = new ModelAndView("books");
-		mav.addObject("booklist", this.bookstore.getBooks());
+		model.addAttribute("booklist", bookstore.getBooks());
 		// application code
-		return mav;
+		return "books";
 	}
 
 	// http://localhost:8080/books/hello
 	
 	@RequestMapping("/hello")
 	@ResponseBody
-	String hello() {
+	public String hello() {
 		System.out.println("BookstoreController.hello()");
 		return "<h2>Hello World!</h2>";
 	}
