8710c6bb2da97ab34f164518d422d03dd23a267e
John Hunt <john.hunt10@gmail.com>
2020-10-08 15:41:01 +0100

    Switched to @SpringBootTest

--- 10-testing/src/test/java/com/jjh/spring/store/BookStoreImplTest.java
+++ 10-testing/src/test/java/com/jjh/spring/store/BookStoreImplTest.java
@@ -2,13 +2,10 @@ package com.jjh.spring.store;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { TestConfig.class} )
+@SpringBootTest(classes = { TestConfig.class} )
 class BookStoreImplTest {
 
     @Autowired
