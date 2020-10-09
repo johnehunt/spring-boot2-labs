9104aea8c37f72772e455e17d41977b4e5d39012
John Hunt <john.hunt10@gmail.com>
2020-10-09 13:30:01 +0100

    Removed change to SpringBootConfiguration back to @Configuration

--- 10-testing/src/test/java/com/jjh/spring/store/TestConfig.java
+++ 10-testing/src/test/java/com/jjh/spring/store/TestConfig.java
@@ -2,7 +2,10 @@ package com.jjh.spring.store;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootConfiguration
-@ComponentScan(basePackages = {"com.jjh.spring.beans","com.jjh.spring.store"})
+@Configuration
+@ComponentScan(basePackages = {
+        "com.jjh.spring.beans",
+        "com.jjh.spring.store"})
 public class TestConfig { }
